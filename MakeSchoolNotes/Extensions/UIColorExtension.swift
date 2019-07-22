a6a70558672cded8aebbbc17db7999318cd1798f
Anna Grace <agrace@exeter.edu>
2019-07-22 10:57:52 -0400

    Initial commit

---
+++ MakeSchoolNotes/Extensions/UIColorExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIColorExtension.swift
+//  MakeSchoolNotes
+//
+//  Created by Anna Grace on 7/5/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import UI
+
+extension UIColor {
+    convenience init(hex: String) {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
